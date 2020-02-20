9d6864c7d33ce272a2ec17e6fa919f0309efca0d
Scott Durant <scott.durant.16@cnu.edu>
2020-02-20 18:36:45 -0500

    figured out how to get multiple inputs to work

--- screens/AddPurchaseScreen.js
+++ screens/AddPurchaseScreen.js
@@ -1,17 +1,45 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Input } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 export default function AddPurchaseScreen() {
+    // useState declares a new state variable called value, and takes an initial state
+    // useState returns two values: the current state and function that updates it
+    const [state, setState] = React.useState({
+        purchaseDescription: "",
+        purchaseAmount: ""
+    })
+
+    function handleChange(event) {
+        setState({ [event.target.name]: event.target.value });
+    }
+
     return (
         <View style={styles.mainContainer}>
             <Text> Add a purchase!</Text>
+            <TextInput
+                name="purchaseDescription"
+                style={{ height: 40 }}
+                placeholder="Enter purchase description:"
+                value={state.purchaseDescription}
+                onChange={handleChange}
+            />
+            <TextInput
+                name="purchaseAmount"
+                keyboardType="number-pad"
+                style={{ height: 40 }}
+                placeholder="Enter purchase amount:"
+                value={state.purchaseAmount}
+                onChange={handleChange}
+            />
+
             <View style={styles.bottomView}>
-                <Button title="Done"/>
+                <Button title="Done" />
             </View>
         </View>
     );
 }
+
 
 
 
