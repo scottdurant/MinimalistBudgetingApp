d9be49c0b7a69f38f7f3d30bb4412b9395b1a844
Scott Durant <scott.durant.16@cnu.edu>
2020-04-15 14:32:03 -0400

    added touchablewithoutfeedback to budget screen

--- screens/BudgetScreen.js
+++ screens/BudgetScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
+import { StyleSheet, Text, View, Button, Keyboard } from 'react-native';
+import { TextInput, TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 export default function AddPurchaseScreen({ navigation }) {
     const [state, setState] = useState({
@@ -73,51 +73,53 @@ export default function AddPurchaseScreen({ navigation }) {
 
     return (
         <View style={styles.container}>
-            <View style={styles.content}>
-                <Text style={styles.text}>Enter the total amount of money you expect to spend this month.</Text>
-                <View>
-                    <TextInput
-                        style={styles.input}
-                        keyboardType={'decimal-pad'}
-                        placeholder='        $0.00        '
-                        value={state.budget}
-                        onChangeText={(text) => setState({ ...state, budget: text })}
-                    />
-                    <Button
-                        title='Update Monthly Budget'
-                        clearTextOnFocus={true}
-                        style={styles.button}
-                        onPress={() => {
-                            budgetSubmitHandler(state.budget)
-                            clearText()
-                        }}
-                    />
+            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+                <View style={styles.content}>
+                    <Text style={styles.text}>Enter the total amount of money you expect to spend this month.</Text>
+                    <View>
+                        <TextInput
+                            style={styles.input}
+                            keyboardType={'decimal-pad'}
+                            placeholder='        $0.00        '
+                            value={state.budget}
+                            onChangeText={(text) => setState({ ...state, budget: text })}
+                        />
+                        <Button
+                            title='Update Monthly Budget'
+                            clearTextOnFocus={true}
+                            style={styles.button}
+                            onPress={() => {
+                                budgetSubmitHandler(state.budget)
+                                clearText()
+                            }}
+                        />
+                    </View>
+                    <View>
+                        <Text style={styles.text}>Create a spending cagetory (i.e. groceries, rent, clothes)</Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder='category name'
+                            value={state.categoryName}
+                            onChangeText={(text) => setState({ ...state, categoryName: text })}
+                        />
+                        <TextInput
+                            style={styles.input}
+                            keyboardType={'decimal-pad'}
+                            placeholder='amount budgeted (monthly)'
+                            value={state.categoryAmountBudgeted}
+                            onChangeText={(text) => setState({ ...state, categoryAmountBudgeted: text })}
+                        />
+                        <Button
+                            title="Submit"
+                            style={styles.button}
+                            onPress={() => {
+                                categorySubmitHandler(state.categoryName, state.categoryAmountBudgeted)
+                                clearText()
+                            }}
+                        />
+                    </View>
                 </View>
-                <View>
-                    <Text style={styles.text}>Create a spending cagetory (i.e. groceries, rent, clothes)</Text>
-                    <TextInput
-                        style={styles.input}
-                        placeholder='category name'
-                        value={state.categoryName}
-                        onChangeText={(text) => setState({ ...state, categoryName: text })}
-                    />
-                    <TextInput
-                        style={styles.input}
-                        keyboardType={'decimal-pad'}
-                        placeholder='amount budgeted (monthly)'
-                        value={state.categoryAmountBudgeted}
-                        onChangeText={(text) => setState({ ...state, categoryAmountBudgeted: text })}
-                    />
-                    <Button
-                        title="Submit"
-                        style={styles.button}
-                        onPress={() => {
-                            categorySubmitHandler(state.categoryName, state.categoryAmountBudgeted)
-                            clearText()
-                        }}
-                    />
-                </View>
-            </View>
+            </TouchableWithoutFeedback>
         </View>
     );
 }
